71c33a9ce93ef85db6d9b363a788f652833d5685
Rita Zhou <rzhou150@gmail.copm>
2024-06-08 16:00:53 -0700

    common layout

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,62 @@
+import {useRef} from "react";
+
 export const Header = ()=> {
-    return <div className="header-container">
-        <h1>Header</h1>
-    </div>
+
+    const menubarWapper = useRef();
+    const navAnimation = (obj)=> {
+        // console.log(obj, obj.className)
+        obj.className = obj.className.replace('fixed', '');
+
+        let topDistance;
+        // Check fixed nav contains any element before get the topDistance
+        if (obj.childNodes) {
+
+            //console.log('has child');
+            topDistance = window.scrollY + obj.getBoundingClientRect().top;
+            console.log('topDistance', topDistance)
+            //console.log(topDistance);
+
+        }
+        let scrollTop = window.scrollY;
+        //console.log(scrollTop);
+        if ( (topDistance) > scrollTop ) {
+            obj.className = obj.className.replace('fixed', '');
+            // document.body.classList.remove('sticky-page-nav');
+            //console.log('not sticky');
+        }
+
+        if ( (topDistance) < scrollTop ) {
+            obj.className = obj.className + ' fixed'
+            // document.body.classList.add('sticky-page-nav');
+            console.log('sticky');
+        }
+    }
+    window.onload = ()=> {
+        // console.log(menubarWapper)
+        navAnimation(menubarWapper.current);
+    };
+
+    window.onresize = ()=> {
+        navAnimation(menubarWapper.current);
+        // console.log(menubarWapper)
+    };
+
+    window.onscroll = ()=> {
+        navAnimation(menubarWapper.current);
+    };
+
+
+
+    return (
+        <header>
+            <div className='header-topbar'></div>
+            <div className="header-menubar-holder">
+                <div ref={menubarWapper} className="header-menubar-wrapper">
+                    {/*<h2>Menu bar</h2>*/}
+                </div>
+            </div>
+
+        </header>
+    )
+
 }
--- src/pages/WhatsNew.js
+++ src/pages/WhatsNew.js
@@ -1,5 +1,16 @@
+import '../assets/css/WhatsNew.scss'
+import {Filter} from "../components/Filter";
+
 export const WhatsNew = ()=> {
-    return <div className="container">
-        <h1>What's new page</h1>
+    return <div className="fluid-container" style={{height:'1000px'}}>
+        <Filter />
+        <div className="main-container">
+            <div className="banner-container">
+                <p className="banner-text">
+                    Goes so easy together.
+                </p>
+
+            </div>
+        </div>
     </div>
 }
