8ccbd069a76af2a4c10f7e1a86567a75034bcf78
Rita Zhou <rzhou150@gmail.copm>
2024-06-08 23:17:32 -0700

    Banner

--- src/pages/WhatsNew.js
+++ src/pages/WhatsNew.js
@@ -1,16 +1,13 @@
 import '../assets/css/WhatsNew.scss'
 import {Filter} from "../components/Filter";
+import {Banner} from "../components/Banner";
 
 export const WhatsNew = ()=> {
+
     return <div className="fluid-container" style={{height:'1000px'}}>
         <Filter />
         <div className="main-container">
-            <div className="banner-container">
-                <p className="banner-text">
-                    Goes so easy together.
-                </p>
-
-            </div>
+            <Banner />
         </div>
     </div>
 }
