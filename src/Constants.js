aedc7e132479208f29b08abb5352ca342dd6e758
Rita Zhou <rzhou150@gmail.copm>
2024-06-10 11:37:06 -0700

    Constants

---
+++ src/Constants.js
@@ -0,0 +1,8 @@
+const Constants = {
+    BASE_URL: 'http://api-lulu.hibitbyte.com',
+    MY_KEY: '',
+    ACTION_FETCH_PRODUCTLIST: 'ACTION_FETCH_PRODUCTLIST',
+    ACTION_FETCH_PRODUCTLIST_NEXTPAGE:'ACTION_FETCH_PRODUCTLIST_NEXTPAGE'
+}
+
+export default Constants
