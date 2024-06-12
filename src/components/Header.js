f81d8d34c8701d5051a588e03cefdb211ee2723f
Rita Zhou <rzhou150@gmail.copm>
2024-06-12 02:27:09 -0700

    header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,17 @@
-import {useRef} from "react";
+import '../assets/css/Header.scss'
+import {useEffect, useRef, useState} from "react";
+import PinDropOutlinedIcon from '@mui/icons-material/PinDropOutlined';
+import CardGiftcardOutlinedIcon from '@mui/icons-material/CardGiftcardOutlined';
+import HelpOutlineOutlinedIcon from '@mui/icons-material/HelpOutlineOutlined';
+import LanguageOutlinedIcon from '@mui/icons-material/LanguageOutlined';
+import {ChildMenus} from "./ChildMenus";
 
 export const Header = ()=> {
 
     const menubarWapper = useRef();
+    const [hoverMenu, setHoverMenu] = useState(0)
+    const [menuList, setMenuList] = useState([])
+
     const navAnimation = (obj)=> {
         // console.log(obj, obj.className)
         obj.className = obj.className.replace('fixed', '');
@@ -10,49 +19,164 @@ export const Header = ()=> {
         let topDistance;
         // Check fixed nav contains any element before get the topDistance
         if (obj.childNodes) {
-
-            //console.log('has child');
             topDistance = window.scrollY + obj.getBoundingClientRect().top;
-            // console.log('topDistance', topDistance)
-            //console.log(topDistance);
-
         }
         let scrollTop = window.scrollY;
-        //console.log(scrollTop);
         if ( (topDistance) > scrollTop ) {
             obj.className = obj.className.replace('fixed', '');
-            // document.body.classList.remove('sticky-page-nav');
-            //console.log('not sticky');
         }
 
         if ( (topDistance) < scrollTop ) {
             obj.className = obj.className + ' fixed'
-            // document.body.classList.add('sticky-page-nav');
-            // console.log('sticky');
         }
     }
     window.onload = ()=> {
-        // console.log(menubarWapper)
         navAnimation(menubarWapper.current);
     };
 
     window.onresize = ()=> {
         navAnimation(menubarWapper.current);
-        // console.log(menubarWapper)
     };
 
     window.onscroll = ()=> {
         navAnimation(menubarWapper.current);
     };
 
+    const handleHoverMenu = (parent)=> {
+        setHoverMenu(parent)
+    }
+    const handleUnhoverMenu = ()=> {
+        setHoverMenu(0)
+    }
+
+    useEffect(() => {
+        fetch('./data/menu.json')
+            .then(resp => resp.json())
+            .then(res => {
+                setMenuList(res.menus)
+            })
+    }, []);
 
 
     return (
         <header>
-            <div className='header-topbar'></div>
+            <div className='header-menu'>
+                <div className='menu-item'>
+                    <PinDropOutlinedIcon sx={{paddingTop:'4px'}}/>
+                    <a href="#">Store Locator</a>
+                </div>
+                <div className='menu-item'>
+                    <CardGiftcardOutlinedIcon sx={{paddingTop:'4px'}}/>
+                    <a href="#">Gift Cards</a>
+                </div>
+                <div className='menu-item'>
+                    <HelpOutlineOutlinedIcon sx={{paddingTop:'4px'}}/>
+                    <a href="#">Get Help</a>
+                </div>
+                <div className='menu-item'>
+                    <LanguageOutlinedIcon sx={{paddingTop:'4px'}}/>
+                    <a href="#">USA</a>
+                </div>
+            </div>
             <div className="header-menubar-holder">
                 <div ref={menubarWapper} className="header-menubar-wrapper">
-                    {/*<h2>Menu bar</h2>*/}
+                    <div className="header-menubar">
+                        <div>
+                            <img className='logo'
+                                 src="https://upload.wikimedia.org/wikipedia/commons/2/22/Lululemon_Athletica_logo.svg"
+                                 alt="logo"/>
+                        </div>
+
+                        <ul>
+                            {menuList.filter(menu=>menu.parent==0).map(rootMenu=> {
+                                return <r key={rootMenu.id}>
+                                    <li onMouseEnter={() => {handleHoverMenu(rootMenu.id)}}
+                                        onMouseLeave={handleUnhoverMenu}> {rootMenu.name}
+                                    </li>
+                                    <ChildMenus handleMouseEnter={() => {handleHoverMenu(rootMenu.id) }}
+                                        handleMouseLeave={handleUnhoverMenu}
+                                        parent={rootMenu.id}
+                                        display={hoverMenu === rootMenu.id}
+                                        title={rootMenu.name}
+                                        submenus={menuList.filter(menu=>menu.rootMenu==rootMenu.id)}
+                                        advertisement={rootMenu.adv}
+                                        activities={rootMenu.activity}/>
+                                </r>
+                            })}
+                            {/*<li*/}
+                            {/*    onMouseEnter={() => {*/}
+                            {/*        handleHoverMenu(1000)*/}
+                            {/*    }}*/}
+                            {/*    onMouseLeave={handleUnhoverMenu}>women*/}
+                            {/*</li>*/}
+                            {/*<ChildMenus*/}
+                            {/*    handleMouseEnter={() => {*/}
+                            {/*        handleHoverMenu(1000)*/}
+                            {/*    }}*/}
+                            {/*    handleMouseLeave={handleUnhoverMenu}*/}
+                            {/*    parent={1000}*/}
+                            {/*    display={hoverMenu === 1000}*/}
+                            {/*    title='women'/>*/}
+
+                            {/*<li onMouseEnter={() => {*/}
+                            {/*    handleHoverMenu(1001)*/}
+                            {/*}}*/}
+                            {/*    onMouseLeave={handleUnhoverMenu}>men*/}
+                            {/*</li>*/}
+                            {/*<ChildMenus*/}
+                            {/*    handleMouseEnter={() => {*/}
+                            {/*        handleHoverMenu(10001)*/}
+                            {/*    }}*/}
+                            {/*    handleMouseLeave={handleUnhoverMenu}*/}
+                            {/*    parent={1001} display={hoverMenu === 1001} title='MEN'/>*/}
+
+                            {/*<li onMouseEnter={() => {*/}
+                            {/*    handleHoverMenu(1002)*/}
+                            {/*}}*/}
+                            {/*    onMouseLeave={handleUnhoverMenu}>accessories*/}
+                            {/*</li>*/}
+                            {/*<ChildMenus*/}
+                            {/*    handleMouseEnter={() => {*/}
+                            {/*        handleHoverMenu(1002)*/}
+                            {/*    }}*/}
+                            {/*    handleMouseLeave={handleUnhoverMenu}*/}
+                            {/*    parent={1002} display={hoverMenu === 1002} title='accessories'/>*/}
+
+                            {/*<li onMouseEnter={() => {*/}
+                            {/*    handleHoverMenu(1003)*/}
+                            {/*}}*/}
+                            {/*    onMouseLeave={handleUnhoverMenu}>shoes*/}
+                            {/*</li>*/}
+                            {/*<ChildMenus*/}
+                            {/*    handleMouseEnter={() => {*/}
+                            {/*        handleHoverMenu(1003)*/}
+                            {/*    }}*/}
+                            {/*    handleMouseLeave={handleUnhoverMenu}*/}
+                            {/*    parent={1003} display={hoverMenu === 1003} title='shoes'/>*/}
+
+                            {/*<li onMouseEnter={() => {*/}
+                            {/*    handleHoverMenu(1004)*/}
+                            {/*}}*/}
+                            {/*    onMouseLeave={handleUnhoverMenu}>father's day*/}
+                            {/*</li>*/}
+                            {/*<ChildMenus*/}
+                            {/*    handleMouseEnter={() => {*/}
+                            {/*        handleHoverMenu(1004)*/}
+                            {/*    }}*/}
+                            {/*    handleMouseLeave={handleUnhoverMenu}*/}
+                            {/*    parent={1004} display={hoverMenu === 1004} title="father's day"/>*/}
+                        </ul>
+                    </div>
+
+
+                    {/*<div className='header-bar-fetch'>*/}
+                    {/*    <div className="header-bar-fetch-item">*/}
+                    {/*        <a href="#">women</a>*/}
+                    {/*    </div>*/}
+                    {/*    <div className="header-bar-fetch-item">*/}
+                    {/*        <a href="#">men</a>*/}
+                    {/*    </div>*/}
+                    {/*</div>*/}
                 </div>
             </div>
 
