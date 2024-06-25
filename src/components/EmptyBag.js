4ddbed40ca1d0432d7ba30f2475c49f0d205d2a2
Heng Zhang <hengzhang@MacBook-Air-M3.local>
2024-06-24 20:08:54 -0700

    footer and empty bag

--- src/components/EmptyBag.js
+++ src/components/EmptyBag.js
@@ -2,9 +2,12 @@ import '../assets/css/EmptyBag.scss'
 
 export const EmptyBag = () => {
     return (
-        <div>
-            <h1>Give your bag some love!</h1>
-            <button>SHOP WHAT'S NEW</button>
+        <div className="emptyBag">
+            <div className='bag'>
+                <h1>Give your bag some love!</h1>
+                <button>SHOP WHAT'S NEW</button>
+            </div>
+
         </div>
     )
 }
