c58bf789f80ea8d341cd9efbe627455e1c056124
李杏 <lixing@lixingdeMacBook-Pro.local>
2024-07-02 15:47:20 -0700

    create

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -17,20 +17,20 @@ export const Checkout = ()=> {
                 <div className="col-1">
                     <ContactInformation/>
                     <CheckoutThree/>
-                    <div className="no-login-block">
-                        <font color='red'> Only display when user not logged in</font>
-                        <img src={notLogin} width='500px' alt=""/>
-                    </div>
-                    <div className="shipping-block">
-                        <div className="shipping-no-login">
-                            <font color='red'>Display when user not logged in</font>
-                            <img src={shippingNotLogin} width='500px' alt=""/>
-                        </div>
-                        <div className="shipping-login">
-                            <font color='red'>Display when user logged in</font>
-                            <img src={shippingAfterLogin} width='500px' alt=""/>
-                        </div>
-                    </div>
+                    {/*<div className="no-login-block">*/}
+                    {/*    <font color='red'> Only display when user not logged in</font>*/}
+                    {/*    <img src={notLogin} width='500px' alt=""/>*/}
+                    {/*</div>*/}
+                    {/*<div className="shipping-block">*/}
+                    {/*    <div className="shipping-no-login">*/}
+                    {/*        <font color='red'>Display when user not logged in</font>*/}
+                    {/*        <img src={shippingNotLogin} width='500px' alt=""/>*/}
+                    {/*    </div>*/}
+                    {/*    <div className="shipping-login">*/}
+                    {/*        <font color='red'>Display when user logged in</font>*/}
+                    {/*        <img src={shippingAfterLogin} width='500px' alt=""/>*/}
+                    {/*    </div>*/}
+                    {/*</div>*/}
                 </div>
 
                 <div className='col-2'>
